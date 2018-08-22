ee480a353303bb1e7193c88abf89447dbeb89a3b
yonathanseleshi <yonathan.seleshi@gmail.com>
2018-08-22 10:13:29 -0700

    Phoneword app demo completed

---
+++ Phoneword/Phoneword.iOS/PhoneDialer.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using Phoneword;
+using Phoneword.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace Phoneword.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
